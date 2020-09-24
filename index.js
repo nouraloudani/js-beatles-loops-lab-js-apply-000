d448d3d98c42313f3b8e72940a72a5324618f43d
Noura Loudani <contact.nouraloudani@gmail.com>
2020-09-24 05:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var whoPlays=[];
+  // for loop( declare your counter variable; condition for continuing; how to increment your counter)
+  for(let i = 0; i < musicians.length; i++){
+    whoPlays.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return whoPlays;
+}
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+theBeatlesPlay(musicians, instruments);
+
+function johnLennonFacts() {
+  var result=[];
+  while(i++) {
+   Result.push(facts[i] + "!!!");
+   }
+  return result;
+}
+
+   const facts = [
+      "He was the last Beatle to learn to drive",
+      "He was never a vegetarian",
+      "He was a choir boy and boy scout",
+      "He hated the sound of his own voice"
+    ];
+    johnLennonFacts();
